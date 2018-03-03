983488feecf8f9f76b2ab1ab8f570ad03c9ccf55
Sergey <34619807+SerjVankovich@users.noreply.github.com>
2018-03-03 10:33:48 +1000

    Add files via upload

--- shop_parser.py
+++ shop_parser.py
@@ -1,5 +1,6 @@
 from bs4 import BeautifulSoup
 import urllib.request
+import openpyxl
 
 def get_html(url):
     response = urllib.request.urlopen(url)
@@ -55,13 +56,26 @@ def main():
         parse(get_html('http://martika-home.ru/catalog/list.php?SECTION_ID=150&PAGEN_1=' + str(first_page)), garden_products)
         first_page += 1
 
-    first_page = 1
     parse(get_html('http://martika-home.ru/catalog/list.php?SECTION_ID=149'), winter)
-    print(winter)
+    save_in_excel(hos_bit_products, 'HosTovars')
+    save_in_excel(kitchen_products, 'KitchenTovars')
+    save_in_excel(garden_products, 'GardenTovars')
+    save_in_excel(winter, 'WinterTovars')
 
 
 
-
+def save_in_excel(obj, list_name):
+    wb = openpyxl.load_workbook(filename = 'C:/Users/sergey/Documents/Products.xlsx')
+    sheet = wb[list_name]
+    i = 2
+    for dict in obj:
+        for key, value in dict.items():
+            if key == 'title':
+                sheet.cell(row=i, column=1).value = value
+            if key == 'articul':
+                sheet.cell(row=i, column=2).value = value
+        i+=1
+    wb.save(r'C:/Users/sergey/Documents/Products.xlsx')
 
 
 
